95cb53090e90ae52f47dd8dd6ee10f76d2a7c28a
Vee Upatising <veeupatising@gmail.com>
2020-07-28 10:29:36 +0800

    Loading Alerts

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Controls-Frenzy/precache-manifest.f95c76cdcc9a5ada20a79724a565c1e7.js"
+  "/Controls-Frenzy/precache-manifest.0a1015e9e1996ddabbb9a6e3c58a3635.js"
 );
 
 self.addEventListener('message', (event) => {
