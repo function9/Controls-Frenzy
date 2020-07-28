d44e7594ae90fedd57b54db71144070b7a8dbd2f
Vee Upatising <veeupatising@gmail.com>
2020-07-28 10:46:26 +0800

    Progress Bar Test

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Controls-Frenzy/precache-manifest.315f14088100be3a00a3d7ce0ba7fef9.js"
+  "/Controls-Frenzy/precache-manifest.be82f8e961f6f1ce90f83c129aa618ce.js"
 );
 
 self.addEventListener('message', (event) => {
