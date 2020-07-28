f0bc938ba23be873f98856d410a93032e9a23334
Vee Upatising <veeupatising@gmail.com>
2020-07-28 09:35:07 +0800

    React Test

---
+++ precache-manifest.8def1b76b023c3528344dcae7dc548cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcab904fbb36ed4ef4c9426f42ba4c4",
+    "url": "/Controls-Frenzy/index.html"
+  },
+  {
+    "revision": "a540456fc50aafaa7239",
+    "url": "/Controls-Frenzy/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "f520807c6d588d2e007b",
+    "url": "/Controls-Frenzy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a540456fc50aafaa7239",
+    "url": "/Controls-Frenzy/static/js/2.7e8379a9.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Controls-Frenzy/static/js/2.7e8379a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f520807c6d588d2e007b",
+    "url": "/Controls-Frenzy/static/js/main.25492c5e.chunk.js"
+  },
+  {
+    "revision": "3b89145d7db56e7e6781",
+    "url": "/Controls-Frenzy/static/js/runtime-main.6b65694c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Controls-Frenzy/static/media/logo.5d5d9eef.svg"
+  }
+]);
